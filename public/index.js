2fc8321cd550aa0f5a4f8158299044eb9918f7dc
koDaegon <daegon7897@gmail.com>
2019-11-11 19:09:42 +0900

    Update ServiceWorker for PWA using workbox

---
+++ public/index.js
@@ -0,0 +1,54 @@
+const API_KEY = '38d6768849d54adcb26773faeb4f034c';
+
+//Resgistering ServiceWork 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('sw.js')
+          .then((reg) => console.log('Service worker registered.', reg));
+    });
+  }
+
+//Add Install button
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  deferredPrompt = event;
+});
+
+const btnAdd = document.getElementById('btnAdd');
+
+btnAdd.addEventListener('click', () => deferredPrompt.prompt());
+
+
+//get the data
+function getNews () {
+    fetch(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`)
+    .then( response => response.json())
+    .then( results => renderNews(results.articles) )    
+}
+//passing object in argument for good practice
+function renderCard (title , imgLink , date , urlLink) {
+    return `
+        <div class ='card'>
+        <a href="${urlLink}"><img src=${imgLink} alt="" class="card-img-top"></a>
+            <div class="card-body">
+            <h5 class="card-title">${title}</h5>   
+                <p class= "card-text> <small class= "text-muted">${date}</small></p>
+             </div>
+        </div>
+    `
+}
+
+function renderNews(news) {
+    const cols = document.getElementById('cardContainer');
+
+    news.map(article => {
+        if(article.urlToImage !== null) {
+            return cols.innerHTML += renderCard(article.title, article.urlToImage, article.publishedAt , article.url);
+        }
+    })
+}
+
+window.addEventListener('DOMContentLoaded' , () => {
+    getNews();
+});
--- public/sw.js
+++ public/sw.js
@@ -38,18 +38,55 @@
 // //caches is built for archving the datas from all the fetch event
 
 
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-if(workbox) {
-    console.log('workbox is loaded');
+if (workbox) {
+  console.log(`Workbox is loaded`);
 }
 
 workbox.routing.registerRoute(
-    new RegExp('^https://newsapi.org')
-)
+    new RegExp('^https://newsapi.org'),
+
+    new workbox.strategies.CacheFirst({
+      cacheName: 'api-cache',
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200],
+        })
+      ]
+    })
+);
 
 workbox.routing.registerRoute(
-    /\.(?:js/css/html)$/,
-    new workbox.regi;
-)
+    /\.(?:js|css|html)$/,
+    new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'images',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        }),
+      ],
+    })
+  );
+
+workbox.routing.registerRoute(
+    /^https:\/\/fonts\.gstatic\.com/,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'google-fonts-webfonts',
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200],
+        }),
+        new workbox.expiration.Plugin({
+          maxAgeSeconds: 60 * 60 * 24 * 365,
+          maxEntries: 30,
+        }),
+      ],
+    })
+);
